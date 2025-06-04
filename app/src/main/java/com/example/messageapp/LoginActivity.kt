f33fa872929df0e1257ab485fe1eea7052aafe56
tiago <tigdrt@gmail.com>
2025-06-04 17:01:11 -0300

    creating a toolbar and register activity navigation

--- app/src/main/java/com/example/messageapp/LoginActivity.kt
+++ app/src/main/java/com/example/messageapp/LoginActivity.kt
@@ -1,20 +1,36 @@
 package com.example.messageapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.messageapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initializeOnClickFunctions()
+    }
+
+    private fun initializeOnClickFunctions() {
+        binding.txtSignUp.setOnClickListener {
+            startActivity(
+                Intent(this, RegisterActivity::class.java)
+            )
         }
     }
 }
--- app/src/main/java/com/example/messageapp/RegisterActivity.kt
+++ app/src/main/java/com/example/messageapp/RegisterActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.messageapp.databinding.ActivityLoginBinding
+import com.example.messageapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initializeToolbar()
+    }
+
+    private fun initializeToolbar() {
+        val toolbar = binding.includeTbRegister.tbRegister
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = ""
+            setDisplayHomeAsUpEnabled(true)
         }
     }
 }
