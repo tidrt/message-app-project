3551c2ab9eb2f071236af03b298b594fceab8762
tiago <dev.tidrt@gmail.com>
2025-06-09 14:31:39 -0300

    verifying logged user, create sign in method and sign out to test

--- app/src/main/java/com/example/messageapp/LoginActivity.kt
+++ app/src/main/java/com/example/messageapp/LoginActivity.kt
@@ -7,10 +7,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.messageapp.databinding.ActivityLoginBinding
+import com.example.messageapp.utils.showMessage
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private lateinit var email : String
+    private lateinit var password : String
+
+    override fun onStart() {
+        super.onStart()
+        verifyUserLogged()
+    }
+
+    private fun verifyUserLogged() {
+        val currentUser = auth.currentUser
+        if(currentUser != null) {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,15 +45,56 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         initializeOnClickFunctions()
     }
 
     private fun initializeOnClickFunctions() {
+        binding.btnLogin.setOnClickListener {
+            if(validateData()){
+                userLogin()
+            }
+        }
+
         binding.txtSignUp.setOnClickListener {
             startActivity(
                 Intent(this, RegisterActivity::class.java)
             )
         }
+    }
+
+    private fun validateData(): Boolean {
+        email = binding.editTextLoginEmail.text.toString()
+        password = binding.editTextLoginPassword.text.toString()
+
+        if(email.isNotEmpty()){
+            binding.editTextLoginEmail.error = null
+            if(password.isNotEmpty()){
+                binding.editTextLoginPassword.error = null
+                return true
+            } else {
+                binding.editTextLoginPassword.error = "Preencha sua senha"
+                return false
+            }
+        } else {
+            binding.editTextLoginEmail.error = "Preencha seu e-mail"
+            return false
+        }
+    }
+
+    private fun userLogin() {
+        email = binding.editTextLoginEmail.text.toString()
+        password = binding.editTextLoginPassword.text.toString()
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                startActivity(
+                    Intent(this, MainActivity::class.java)
+                )
+                showMessage("Login realizado com sucesso")
+            }
+            .addOnFailureListener{
+                showMessage("Falha ao realizar o seu login")
+            }
     }
 }
--- app/src/main/java/com/example/messageapp/MainActivity.kt
+++ app/src/main/java/com/example/messageapp/MainActivity.kt
@@ -5,17 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.messageapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initializeOnClickEvents()
+    }
+
+    private fun initializeOnClickEvents() {
+        binding.btnLogOut.setOnClickListener{
+            userSignOut()
+        }
+    }
+
+    private fun userSignOut() {
+        auth.signOut()
+        finish()
     }
 }
