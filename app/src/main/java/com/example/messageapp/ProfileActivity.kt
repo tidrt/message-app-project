1ae46e8b2d43dd4755c7531987825d91d3d47058
tiago <dev.tidrt@gmail.com>
2025-06-15 23:01:46 -0300

    creating the profile activity

--- app/src/main/java/com/example/messageapp/ProfileActivity.kt
+++ app/src/main/java/com/example/messageapp/ProfileActivity.kt
@@ -5,16 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.messageapp.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityProfileBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initializeToolbar()
+    }
+
+    private fun initializeToolbar() {
+        val toolbar = binding.includeTbProfile.tbRegister
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Perfil"
+            setDisplayHomeAsUpEnabled(true)
         }
     }
 }
