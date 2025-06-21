a97932474d8939761fb0fbc579adbbff49e34881
tiago <dev.tidrt@gmail.com>
2025-06-21 11:14:28 -0300

    permissions request and verification

--- app/src/main/java/com/example/messageapp/ProfileActivity.kt
+++ app/src/main/java/com/example/messageapp/ProfileActivity.kt
@@ -1,8 +1,13 @@
 package com.example.messageapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.messageapp.databinding.ActivityProfileBinding
@@ -11,6 +16,9 @@ class ProfileActivity : AppCompatActivity() {
     private val binding by lazy{
         ActivityProfileBinding.inflate(layoutInflater)
     }
+
+    private var camPermission = false
+    private var galleryPermission = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +29,37 @@ class ProfileActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         initializeToolbar()
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        camPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        galleryPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val deniedPermissions = mutableListOf<String>()
+        if(!camPermission){
+            deniedPermissions.add(Manifest.permission.CAMERA)
+        }
+        if(!galleryPermission){
+            deniedPermissions.add(Manifest.permission.CAMERA)
+        }
+
+        if(deniedPermissions.isNotEmpty()){
+            val permissionsManager = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ){ permission ->
+                camPermission = permission[Manifest.permission.CAMERA] ?: camPermission
+                galleryPermission = permission[Manifest.permission.READ_EXTERNAL_STORAGE] ?: galleryPermission
+            }.launch(deniedPermissions.toTypedArray())
+        }
     }
 
     private fun initializeToolbar() {
