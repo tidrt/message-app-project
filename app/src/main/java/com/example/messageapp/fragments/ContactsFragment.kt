1fcb2503208dcea684080eb11958c2267ddda911
tiago <dev.tidrt@gmail.com>
2025-06-30 15:31:00 -0300

    creating new users and list all users in the contacts fragment

--- app/src/main/java/com/example/messageapp/fragments/ContactsFragment.kt
+++ app/src/main/java/com/example/messageapp/fragments/ContactsFragment.kt
@@ -1,19 +1,59 @@
 package com.example.messageapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.messageapp.R
+import com.example.messageapp.databinding.FragmentContactsBinding
+import com.example.messageapp.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class ContactsFragment : Fragment() {
+    private lateinit var binding : FragmentContactsBinding
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false)
+        binding = FragmentContactsBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        contactsListener()
+    }
+
+    private fun contactsListener() {
+        firestore
+            .collection("users")
+            .addSnapshotListener { querySnapshot, error ->
+                val documents = querySnapshot?.documents
+                documents?.forEach { documentSnapshot ->
+                    val user = documentSnapshot.toObject(User::class.java)
+                    if(user != null){
+                        Log.i("contact_listener", "nome: ${user.name}")
+                    }
+                }
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
