0fabcfe9da8682ced158adc7c2ed7e057fbd8488
tiago <dev.tidrt@gmail.com>
2025-07-04 12:37:05 -0300

    chat message ui created

--- app/src/main/java/com/example/messageapp/activities/MessageActivity.kt
+++ app/src/main/java/com/example/messageapp/activities/MessageActivity.kt
@@ -10,6 +10,7 @@ import com.example.messageapp.R
 import com.example.messageapp.databinding.ActivityMessageBinding
 import com.example.messageapp.model.User
 import com.example.messageapp.utils.Constants
+import com.squareup.picasso.Picasso
 
 class MessageActivity : AppCompatActivity() {
 
@@ -28,8 +29,24 @@ class MessageActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         recoverRecipientData()
+        initializeToolbar()
+    }
+
+    private fun initializeToolbar() {
+        val toolbar = binding.mtMessageToolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = ""
+            if(recipientData != null){
+                Picasso.get()
+                    .load(recipientData!!.photo)
+                    .into(binding.imgMessageRecipientProfilePhoto)
+
+                binding.txtMessageRecipientProfileName.text = recipientData!!.name
+            }
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 
     private fun recoverRecipientData() {
@@ -43,7 +60,7 @@ class MessageActivity : AppCompatActivity() {
                     recipientData = extras.getParcelable("recipientData")
                 }
             } else if(source == Constants.SOURCE_CHAT){
-                
+
             }
         }
     }
